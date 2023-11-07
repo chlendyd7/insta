b9902919bcd731943277fe9bf2000d1e610040e4
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2023-11-07 14:33:33 +0900

    react-router-dom을 활용한 라우팅 처리

---
+++ backend/test.py
@@ -0,0 +1,20 @@
+import requests
+from requests import utils
+from urllib.parse import unquote
+api_key = "/OuacqJ8UUETaMvmR/YU8n3Ne4K3K6vhGBT1oq/xTmKOUoM44X6O5gc56//14W+AZFaXfM7ldALKcGvL8zWLEg=="
+
+url = 'http://www.culture.go.kr/openapi/rest/publicperformancedisplays/period'
+paramss ={'serviceKey' : api_key, 'keyword' : '', 'sortStdr' : '1', 'ComMsgHeader' : '', 'RequestTime' : '20100810:23003422', 'CallBackURI' : '', 'MsgBody' : '', 'from' : '20100101', 'to' : '20101201', 'cPage' : '1', 'rows' : '10', 'place' : '1', 'gpsxfrom' : '129.101', 'gpsyfrom' : '35.142', 'gpsxto' : '129.101', 'gpsyto' : '35.142' }
+
+response = requests.request('GET', url, params=paramss)
+print(response.content)
+
+# import requests
+# api_key = "/OuacqJ8UUETaMvmR/YU8n3Ne4K3K6vhGBT1oq/xTmKOUoM44X6O5gc56//14W+AZFaXfM7ldALKcGvL8zWLEg=="
+
+# url = 'http://apis.data.go.kr/6260000/BusanBIMS/busStopList'
+# paramss ={'serviceKey':api_key, 'pageNo' : '1', 'numOfRows' : '10', 'bstopnm' : '부산시청', 'arsno' : '13708' }
+
+# response = requests.request('GET', url, params=paramss)
+# print(response.content)
+
