74f7bdfe6ca2fbb549a79e77826d6f701276f08c
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2023-11-12 21:54:56 +0900

    프론트 Sign up 기능

--- backend/test.py
+++ backend/test.py
@@ -3,11 +3,11 @@ from requests import utils
 from urllib.parse import unquote
 api_key = "/OuacqJ8UUETaMvmR/YU8n3Ne4K3K6vhGBT1oq/xTmKOUoM44X6O5gc56//14W+AZFaXfM7ldALKcGvL8zWLEg=="
 
-url = 'http://www.culture.go.kr/openapi/rest/publicperformancedisplays/period'
-paramss ={'serviceKey' : api_key, 'keyword' : '', 'sortStdr' : '1', 'ComMsgHeader' : '', 'RequestTime' : '20100810:23003422', 'CallBackURI' : '', 'MsgBody' : '', 'from' : '20100101', 'to' : '20101201', 'cPage' : '1', 'rows' : '10', 'place' : '1', 'gpsxfrom' : '129.101', 'gpsyfrom' : '35.142', 'gpsxto' : '129.101', 'gpsyto' : '35.142' }
+# url = 'http://www.culture.go.kr/openapi/rest/publicperformancedisplays/period'
+# paramss ={'serviceKey' : api_key, 'keyword' : '', 'sortStdr' : '1', 'ComMsgHeader' : '', 'RequestTime' : '20100810:23003422', 'CallBackURI' : '', 'MsgBody' : '', 'from' : '20100101', 'to' : '20101201', 'cPage' : '1', 'rows' : '10', 'place' : '1', 'gpsxfrom' : '129.101', 'gpsyfrom' : '35.142', 'gpsxto' : '129.101', 'gpsyto' : '35.142' }
 
-response = requests.request('GET', url, params=paramss)
-print(response.content)
+# response = requests.request('GET', url, params=paramss)
+# print(response.content)
 
 # import requests
 # api_key = "/OuacqJ8UUETaMvmR/YU8n3Ne4K3K6vhGBT1oq/xTmKOUoM44X6O5gc56//14W+AZFaXfM7ldALKcGvL8zWLEg=="
@@ -18,3 +18,13 @@ print(response.content)
 # response = requests.request('GET', url, params=paramss)
 # print(response.content)
 
+
+# Python3 샘플 코드 #
+
+import requests
+
+url = 'http://apis.data.go.kr/6260000/BusanCultureExhibitService/getBusanCultureExhibit'
+params ={'serviceKey' : api_key, 'pageNo' : '1', 'numOfRows' : '10' }
+
+response = requests.get(url, params=params)
+print(response.content['pay_at'])
---
+++ backend/xml.py
@@ -0,0 +1,11 @@
+# import xml.etree.ElementTree as ET
+
+# import requests
+# from test import api_key
+
+# url = 'http://apis.data.go.kr/6260000/BusanCultureExhibitService/getBusanCultureExhibit'
+# params ={'serviceKey' : api_key, 'pageNo' : '1', 'numOfRows' : '10' }
+
+# response = requests.get(url, params=params)
+# print(response.content)
+
